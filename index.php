590c645c4d56aeeb08749e9f08a2301f6374785e
petetav <petethepilot69@gmail.com>
2017-01-09 22:55:33 +0000

    catchup

---
+++ index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<style>
+	</style>	
+<meta charset="utf-8">
+
+  <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame 
+       Remove this if you use the .htaccess -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title>Lopud Villas</title>
+  <meta name="description" content="">
+  <meta name="author" content="Pete">
+
+
+<!-- favicon coutesy of http://www.favicon.cc/ -->
+<link href="img/sun_favicon.ico" rel="icon" type="image/x-icon" />
+
+
+
+  <!--  Mobile viewport optimized: j.mp/bplateviewport -->
+  <meta name="viewport" content="width=device-width; initial-scale=1.0">
+    
+  <!-- CSS : implied media="all" -->
+  <link rel='stylesheet' type="text/css" href="css/new.css">
+  
+  <!-- All JavaScript at the bottom, except for Modernizr which enables HTML5 elements & feature detects -->
+  <script src="js/modernizr2.6.2.js"></script>
+
+
+</head>
+<body>
+	
+	
+	
+	<section class="title">
+		<h1>Lopud Villas</h1>
+	</section>
+	
+	<section class="spacephoto">
+		<img src="img/lopud_from_space400x637.png" alt="lopud_from_space" width="100%">
+	</section>
+
+		
+	
+	<section></section>
+	<section></section>
+	
+	
+</body>
+</html>
