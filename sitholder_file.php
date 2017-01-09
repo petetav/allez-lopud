ca8315f2ab7d8d849e07acae2edff800ebab1099
petetav <petethepilot69@gmail.com>
2017-01-09 22:44:20 +0000

    catchup

---
+++ sitholder_file.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+  <!--[if lt IE 7]> <html lang="en" class="lt-ie10 lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+  <!--[if IE 7]>    <html lang="en" class="lt-ie10 lt-ie9 lt-ie8"> <![endif]-->
+  <!--[if IE 8]>    <html lang="en" class="lt-ie10 lt-ie9"> <![endif]-->
+  <!--[if IE 9]>    <html lang="en" class="lt-ie10"> <![endif]-->
+  <!--[if gt IE 10]><!--> <html lang="en"> <!--<![endif]-->
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>This domain has been registered | 34SP.com</title>
+    <meta name="description" content="Unfortunately the site you are looking for is unavailable at this time.">
+    <link rel="shortcut icon" href="//34sp.net/img/ico/www/favicon.png" type="image/x-icon" />
+
+    <!-- Bootstrap core CSS -->  
+    <link href="//34sp.net/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles -->
+    <link href="//34sp.net/css/style.css" rel="stylesheet">
+    <link href="//34sp.net/css/www.css" rel="stylesheet">
+    <!-- Font Awesome CSS -->  
+    <link href="//34sp.net/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="//34sp.net/js/html5shiv.js"></script>
+      <script src="//34sp.net/js/respond.min.js"></script>
+
+      <link href="//34sp.net/respond-proxy.html" id="respond-proxy" rel="respond-proxy" />
+    <![endif]-->
+
+    <!-- Font Awesome Support for IE7-->
+    <!--[if lt IE 8]>
+      <link rel="stylesheet" media="all" href="//34sp.net/css/font-awesome.ie7.css" />
+    <![endif]-->
+
+  </head>
+
+  <body>
+    <header id="top">
+
+      <nav id="primary-navigation" class="navbar navbar-default navbar-static-top">
+        <div id="header-navigation">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#primary-navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="fa fa-bars fa-lg"></span>
+              </button>
+              <a class="navbar-brand" href="https://www.34sp.com/"><img src="//34sp.net/img/34sp.png" alt="34SP.com" class="img-responsive"></a>
+            </div>
+
+            <ul class="nav navbar-nav navbar-right hidden-xs">
+              <li class="navbar-text visible-lg">Sales &amp; Support: 0161 987 3434</li>
+              <li><a href="https://www.34sp.com/about">About Us</a></li>
+              <li><a href="https://www.34sp.com/contact">Contact Us</a></li>            
+              <li><a href="http://status.34sp.com" target="_blank">Status</a></li>           
+            </ul>
+          </div>
+        </div>
+        <div class="container">
+          <div id="primary-navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li><a href="https://www.34sp.com/">Home</a></li>
+              <li><a href="https://www.34sp.com/domains">Domains</a></li>
+              <li><a href="https://www.34sp.com/hosting">Hosting</a></li>            
+              <li><a href="https://www.34sp.com/servers">Servers</a></li>
+              <li><a href="https://www.34sp.com/web-hosting-support">Support</a></li>
+              <li><a href="https://account.34sp.com">Login</a></li>          
+            </ul>
+
+            <div vocab="http://schema.org/" typeof="WebSite">
+              <meta property="url" content="https://www.34sp.com/"/>
+              <form method="post" action="https://www.34sp.com/register-domain" property="potentialAction" typeof="http://schema.org/SearchAction" id="navbar-search" class="navbar-form navbar-right visible-lg-block">
+                <meta property="target" content="https://www.34sp.com/search?q={query}"/>
+                
+                <div class="input-group input-group-sm">
+                  <input property="query-input" type="text" class="form-control" name="domain" placeholder="Search to buy a domain name">
+                  <span class="input-group-btn">
+                    <button class="btn btn-default" type="submit"><span class="fa fa-search fa-lg"></span></button>
+                  </span>
+                </div><!-- /input-group -->
+              </form>
+            </div>
+            
+          </div><!--/.nav-collapse -->
+        </div>
+      </nav>
+    </header>
+
+    <section>
+      <div class="container">
+        <h1 class="text-uppercase red">This domain has been registered</h1>
+        <p class="lead">Website hosting for this domain name is provided by 34SP.com</p>
+        <hr>
+
+        <h3>Web Hosting with Superior Service and Technical Support</h3>        
+        <p>34SP.com provides <a href="http://www.34sp.com/hosting">web hosting</a> services for online businesses, web developers, and web designers in the UK and throughout the world. The company's main focus is providing the very best customer service experience for every account holder.</p>        
+        <p>34SP.com offers a wide variety of products for online professionals. <a href="http://www.34sp.com/business-hosting">Business web hosting</a> accounts are ideal for any online business, but most especially for those needing an online store or ecommerce facilities. <a href="http://www.34sp.com/reseller-hosting">Reseller hosting</a> accounts are well suited to web designers and web developers, who create websites for their own clients. For those entrepreneurs and businesses possessing deeper technical skills, both <a href="http://www.34sp.com/vps-hosting">VPS hosting</a> (virtual server environments) and <a href="http://www.34sp.com/dedicated-servers">dedicated servers</a> are available to ensure maximum web hosting uptime and security.</p>          
+        <p>Setting up a simple website or blog is quick and easy, with <a href="http://wordpresshosting.34sp.com">WordPress hosting</a>. The first month is free, and 34SP.com provides a 1 click installer for WordPress installs.</p>          
+        <p>To see continuously updated testimonials and comments on 34SP.com hosting products from real customers please visit our <a href="https://twitter.com/#!/34SP/favorites" target="_blank" rel="nofollow">Twitter page</a>.</p>
+      </div>
+    </section>
+
+    <footer id="site-footer">
+      <div class="container">       
+        <div class="row">
+          <div class="col-md-6">
+            <h5 class="text-uppercase">Get in touch</h5>
+            <p>We offer a range of hosting solutions to suit every circumstance. Please feel free to browse through our website, or get in touch with us directly.</p>
+
+            <address>
+              <div itemscope itemtype="http://schema.org/Organization">
+                <ul class="list-unstyled">
+                  <li><span class="fa fa-fw fa-envelope-o"></span> <a href="mailto:info@34sp.com" itemprop="email">info@34sp.com</a></li>
+                  <li><span class="fa fa-fw fa-phone"></span> <span itemprop="telephone">0161 987 34 34</span></li>
+                  <li><span class="fa fa-fw fa-map-marker"></span> <span itemprop="address" itemscope itemtype="http://schema.org/PostalAddress"><span itemprop="name">34SP.com Ltd</span>, <span itemprop="streetAddress">37 Turner Street</span>, <span itemprop="addressRegion">Manchester</span>, <span itemprop="addressCountry">UK</span>, <span itemprop="postalCode">M4 1DW</span></span></li>
+                </ul>
+              </div>
+            </address>
+          </div>
+          <hr class="visible-sm-block visible-xs-block">
+          <div class="col-md-6">
+            <h5 class="text-uppercase">Get Social</h5>
+            <p>Get more from 34SP.com, by checking out our social media channels &amp; feeds.</p>
+            <ul class="list-inline list-social">
+              <li class="twitter"><a href="https://twitter.com/34sp" target="_blank" class="fa fa-fw fa-twitter"></a></li>
+              <li class="facebook"><a href="https://www.facebook.com/pages/34SP/217495734942668" target="_blank" class="fa fa-fw fa-facebook"></a></li>
+              <li class="google-plus"><a href="https://plus.google.com/117191666533773878594/posts" target="_blank" class="fa fa-fw fa-google-plus"></a></li>
+              <li class="flickr"><a href="https://www.flickr.com/34sp/" target="_blank" class="fa fa-fw fa-flickr"></a></li>
+              <li class="rss"><a href="https://www.34sp.com/hosting-news/feed/" target="_blank" class="fa fa-fw fa-rss"></a></li>
+            </ul>
+            
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <footer id="site-footer-links">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-md-2 col-sm-4 col-xs-6">
+            <h5 class="text-uppercase"><a href="https://www.34sp.com/hosting">Hosting</a></h5>
+            <ul class="list-unstyled">
+              <li><a href="https://www.34sp.com/professional-hosting">Professional Hosting</a></li>
+              <li><a href="https://www.34sp.com/business-hosting">Business Hosting</a></li>
+              <li><a href="https://www.34sp.com/reseller-hosting">Reseller Hosting</a></li>
+              <li><a href="https://www.34sp.com/wordpress-hosting">WordPress Hosting</a></li>
+            </ul>
+          </div>
+
+          <div class="col-md-2 col-sm-4 col-xs-6">
+            <h5 class="text-uppercase"><a href="https://www.34sp.com/domains">Domains</a></h5>
+            <ul class="list-unstyled">
+              <li><a href="https://www.34sp.com/domains">Domain Name Search</a></li>
+              <li><a href="https://www.34sp.com/domain-transfer">Transfer Domain</a></li>
+            </ul>
+          </div>
+
+          <div class="clearfix visible-xs-block"></div>
+
+          <div class="col-md-2 col-sm-4 col-xs-6">
+            <h5 class="text-uppercase"><a href="https://www.34sp.com/servers">Servers</a></h5>
+            <ul class="list-unstyled">
+              <li><a href="https://www.34sp.com/vps-hosting">VPS Hosting</a></li>
+              <li><a href="https://www.34sp.com/dedicated-servers">Dedicated Servers</a></li>
+            </ul>
+          </div>
+
+          <div class="clearfix visible-sm-block"></div>
+
+          <div class="col-md-2 col-sm-4 col-xs-6">
+            <h5 class="text-uppercase"><a href="https://www.34sp.com/services">Services</a></h5>
+            <ul class="list-unstyled">
+              <li><a href="https://www.34sp.com/ssl">SSL Certificates</a></li>
+              <li><a href="https://www.34sp.com/smtp-hosting">SMTP Hosting</a></li>
+              <li><a href="https://www.34sp.com/website-backups">Website Backups</a></li>
+              <li><a href="https://www.34sp.com/server-management">Server Management</a></li>
+            </ul>
+          </div>
+
+          <div class="clearfix visible-xs-block"></div>
+
+          <div class="col-md-2 col-sm-4 col-xs-6">
+            <h5 class="text-uppercase"><a href="https://www.34sp.com/about">About</a></h5>
+            <ul class="list-unstyled">
+              <li><a href="https://www.34sp.com/web-hosting-experts">Meet the Team</a></li>
+              <li><a href="https://www.34sp.com/manchester-data-centre">Our Data Centre</a></li>
+              <li><a href="https://www.34sp.com/sitemap">Sitemap</a></li>
+              <li><a href="https://www.34sp.com/administrative">Administrative</a></li>
+            </ul>
+          </div>
+          <div class="col-md-2 col-sm-4 col-xs-6">
+            <h5 class="text-uppercase"><a href="https://www.34sp.com/web-hosting-support">Support</a></h5>
+            <ul class="list-unstyled">
+              <li><a href="https://www.34sp.com/contact">Contact Us</a></li>
+              <li><a href="https://www.34sp.com/web-hosting-support">Knowledge Base</a></li>
+              <li><a href="http://forums.34sp.com" target="_blank">Support Forums</a></li>
+            </ul>
+          </div>
+        </div>       
+
+        <div class="row">
+          <hr>
+          <div class="col-xs-6">
+            <small><span class="fa fa-copyright"></span> 34SP.com 2015. Copyright All Rights Reserved</small>
+          </div>
+          <div class="col-xs-6">
+            <small class="pull-right"><a href="#top">Back to top <span class="fa fa-chevron-circle-up fa-lg"></span></a></small>
+          </div>
+        </div>
+      </div>
+    </footer>
+    
+    <!-- JavaScript
+    ================================================== -->
+
+    <!-- jQuery -->  
+    <script data-minify="true" src="//34sp.net/js/jquery-1.11.0.min.js"></script>
+    <script data-minify="true" src="//34sp.net/js/jquery-ui-1.11.0.custom.min.js"></script>
+    <script data-minify="true" src="//34sp.net/js/jquery-migrate-1.2.1.min.js"></script>
+    <!-- Bootstrap -->
+    <script data-minify="true" src="//34sp.net/js/bootstrap.min.js"></script>  
+    <!-- Custom -->
+    <script data-minify="true" src="//34sp.net/js/common.js"></script>
+    <script data-minify="true" src="//34sp.net/js/www.js"></script>   
+
+  </body>
+</html>
